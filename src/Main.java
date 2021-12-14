4bef105e662d20954733084897f75fc16cbc57ae
Chinye Danielson <chinyedaniel@gmail.com>
2021-12-14 11:40:18 +0000

    line 3 scanner error isn't displaying after removing the getnumber() from the Scanner code

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 public class Main {
     public static void main (String ... args) throws IOException{
         Parser parser = new Parser(new Scanner( new BufferedReader(new FileReader("src/ac_file"))));
+//        Parser parser = new Parser(new Scanner( new BufferedReader(new FileReader("src/ac_test_prog"))));
         parser.run();
 //        System.out.println("Syntax analysis performed successfully and no errors were found.");
     }
